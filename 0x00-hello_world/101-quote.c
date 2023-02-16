43db9134a1cce728ce929eb8aad4424ecec3f5bd
ETOO <youssefashrafeissa@gmail.com>
2023-02-16 12:13:00 -0800

    writing an error message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - starter function
+ * print error message
+ * Return: (1) Error
+ */
+int main(void)
+{
+	char message[100];
+
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, strlen(message));
+	return (1);
+}
