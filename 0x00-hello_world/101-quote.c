f96d213c24e21c35e676538d4bbd4aa10038bc42
ETOO <youssefashrafeissa@gmail.com>
2023-02-16 12:43:39 -0800

    writing an error message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\"",32);
-	write(STDERR_FILENO, " - Dora Korpar, 2015-10-19\n",27);
+	write(STDERR_FILENO, "and that piece of art is useful\"", 32);
+	write(STDERR_FILENO, " - Dora Korpar, 2015-10-19\n", 27);
 	return (1);
 }
