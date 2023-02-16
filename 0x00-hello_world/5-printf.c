1367f34d2f01c26aec7cf822193405f839134f7c
ETOO <youssefashrafeissa@gmail.com>
2023-02-16 08:00:41 -0800

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starter function
+ * print a message
+ * retunr 0 succsses
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
