d4eb588125ab9db938913456acd2a089439b2187
ETOO <youssefashrafeissa@gmail.com>
2023-02-16 12:39:29 -0800

    writing an error message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,13 @@
 #include <unistd.h>
 #include <string.h>
-
-int print(char message[]);
 /**
  * main - starter function
- * print error message
+ * Description: 'print error message'
  * Return: (1) Error
  */
 int main(void)
 {
-       print("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-       return (1);
-}
-/**
- * print message function
- * print the message required
- * Return: (0) Succsses
- */
-int print(char message[])
-{
-	write(STDERR_FILENO, message, strlen(message));
-	return (0);
+	write(STDERR_FILENO, "and that piece of art is useful\"");
+	write(STDERR_FILENO, " - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
