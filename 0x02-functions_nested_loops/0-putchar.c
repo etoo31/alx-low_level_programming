5d520c29a2bc18a90ed8ca540b860fe02f3d7cb0
ETOO <youssefashrafeissa@gmail.com>
2023-02-22 04:01:50 -0800

    Put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	char message[9];
+	char message[] = "_putchar\n";
+	int i;
 
-	message = "_putchar\n";
-	for (int i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(message[i]);
 	}
