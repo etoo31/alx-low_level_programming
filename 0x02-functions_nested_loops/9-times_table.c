a9a4b0d3e4689860ff85c2481039b1528c6b8287
ETOO <youssefashrafeissa@gmail.com>
2023-02-22 14:44:14 -0800

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - print the time table of all numbers till 9
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 10; j++)
+		{
+			int result;
+
+			result = i * j;
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			if (j == 9)
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+	}
+}
