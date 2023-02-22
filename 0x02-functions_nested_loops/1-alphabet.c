cf8ddc0e7f0bcbb94abf22fb680f0c5b4622f663
ETOO <youssefashrafeissa@gmail.com>
2023-02-22 04:10:56 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - function
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	return (0);
+}
