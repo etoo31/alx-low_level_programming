236ba5715c05cdacf8e18190facbaa924db3fcdf
ETOO <youssefashrafeissa@gmail.com>
2023-02-22 15:01:10 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * @n1: first number
+ *
+ * @n2: second number
+ *
+ * Return: return the sum of the two numbers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
