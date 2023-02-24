dada9d5478e5fc9b74733d154ec2dc25b134d033
ETOO <youssefashrafeissa@gmail.com>
2023-02-23 17:27:00 -0800

    print more number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,11 @@ void more_numbers(void)
 
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar('0' + j);
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
 			}
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
