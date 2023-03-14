4f8a5952f77ed8b1ece790382d264976508a8685
ETOO <youssefashrafeissa@gmail.com>
2023-03-14 10:16:50 -0700

    args .c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - starter function
+ *
+ * @argc: number of arguments sent
+ *
+ * @argv: array of arugments
+ *
+ * Return: 0 - succsess
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
